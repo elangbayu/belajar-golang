47037e38e5b4b66b258d9565dd801f63998fdada
elangbayu <elangbayu17@gmail.com>
2023-09-18 20:54:52 +0700

    variables

---
+++ variables/constant.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+  const (
+    firstName string = "Elang"
+    lastName = "Segara"
+    age = 30
+  )
+
+  fmt.Println(firstName)
+  fmt.Println(lastName)
+  fmt.Println(age)
+}
+
---
+++ variables/variable.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+  var name string
+
+  name = "Elang Segara"
+  fmt.Println(name)
+
+  name = "Elang Bayu"
+  fmt.Println(name)
+
+  var surname = "Elang Bayu Segara"
+  fmt.Println(surname)
+
+  var age = 24
+  fmt.Println(age)
+
+  country := "Indonesia"
+  fmt.Println(country)
+
+  var (
+    firstName = "Elang"
+    lastName = "Segara"
+  )
+
+  fmt.Println(firstName)
+  fmt.Println(lastName)
+}
+
