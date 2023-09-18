502d43f2c118e652428265705010724f985f4329
elangbayu <elangbayu17@gmail.com>
2023-09-18 20:33:41 +0700

    tipe data di golang

---
+++ tipe-data/string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Elang")
+  fmt.Println("Bayu")
+  fmt.Println("Segara")
+
+  fmt.Println(len("Elang"))
+  fmt.Println("Bayu"[0])
+  fmt.Println("Segara"[1])
+}
+
